5baa5fe975e0c44d46434c75eacae48a2400e602
Daniel Herskowitz <daniel_herskowitz@mckinsey.com>
2019-05-22 09:37:29 -0400

    taskbox UI

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,10 @@
 import { configure } from '@storybook/react';
 
 
+// import 'react-chromatic/storybook-addon';
+
+
+import 'storybook-chromatic';
 
 
 import requireContext from 'require-context.macro';
---
+++ storybook-static/precache-manifest.816b78ba31d77d2073128fb61da8eca7.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf2e1b6216f9a73a3c37f66103d3fca",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "1d87dd88c330dce48bb8",
+    "url": "main.css"
+  },
+  {
+    "url": "main.fb2cd65997b2d408e744.bundle.js"
+  },
+  {
+    "url": "runtime~main.fb2cd65997b2d408e744.bundle.js"
+  },
+  {
+    "url": "vendors~main.fb2cd65997b2d408e744.bundle.js"
+  }
+]);
